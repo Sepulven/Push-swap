69fe9b2c1d92b584095078d7a7eceac16347b3dd
Sepulven <sepulven40028922@gmail.com>
2023-02-16 13:43:45 +0000

    .

--- Makefile
+++ Makefile
@@ -6,24 +6,39 @@
 #    By: asepulve <asepulve@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/02/08 14:06:30 by asepulve          #+#    #+#              #
-#    Updated: 2023/02/08 14:51:03 by asepulve         ###   ########.fr        #
+#    Updated: 2023/02/08 22:29:04 by asepulve         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
+
+
+#   5 numbers -> <12 moves
+#   100 numbers:
+#       <700 moves = 5pts
+#       <900 numbers = 4pts
+#       <1,100 numbers = 3pts
+#       <1,300 numbers = 2pts
+#       <1,500 numbers = 1pts
+#   500 numbers:
+#       <5,500 moves = 5pts
+#       <7,000 moves = 4pts
+#       <8,500 moves = 3pts
+#       <10,000 moves = 2pts
+#       <11,500 moves = 1pts
 
 NAME 		= 	push_swap
 CC 			= 	cc
 CFLAGS 		= 	-Wall -Wextra -Werror
 RM 			= 	rm -f
 SRC			=	main.c
+OBJ		 	= 	${SRC:.c=.o}
 INCLUDES	=	libft/libft.a
-OBJ		 	= 	${SRC:.o=.c}
-
-.o:.c
-			${CC} ${CFLAGS} $< -c
 
 all:		${NAME}
 
-$(NAME):	
+%.o: %.c
+			$(CC) $(CFLAGS) -c $^ -o $@
+
+$(NAME): ${OBJ}
 			make -s -C libft
 			@${CC} ${CFLAGS} ${OBJ} libft/libft.a -o ${NAME}
 
@@ -36,3 +51,5 @@ fclean:		clean
 			@${RM} ${NAME}
 
 re:			fclean ${NAME}
+
+.PHONY: all clean fclean re
--- main.c
+++ main.c
@@ -6,16 +6,109 @@
 /*   By: asepulve <asepulve@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 14:29:23 by asepulve          #+#    #+#             */
-/*   Updated: 2023/02/08 14:46:04 by asepulve         ###   ########.fr       */
+/*   Updated: 2023/02/08 23:50:04 by asepulve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+/*
+	* 1:	Check num;
+	* 2:	Check duplicate;
+	* 3:	Change to int;
+	* 4:	Pass to the stack;
+	* 5:	Check limit of integer
+*/
+
+int	ft_isnum(char *num)
+{
+	int	i;
+
+	i = 0;
+	if (!ft_isdigit(num[i]))
+	{
+		if (num[i++] != '-')
+			return (0);
+	}
+	while (num[i])
+	{
+		if (!ft_isdigit(num[i++]))
+			return (0);
+	}
+	return (1);
+}
+
+t_list *converter(int argc, char *argv[])
+{
+	t_list	*stack;
+	void	*nbr;
+	int		i;
+
+	i = 1;
+	nbr = malloc(sizeof(int));
+	*(int *)nbr = ft_atoi(argv[i++]);
+	stack = ft_lstnew(nbr);
+	while (i < argc)
+	{
+		nbr = malloc(sizeof(int));
+		*(int *)nbr = ft_atoi(argv[i++]);
+		ft_lstadd_back(&stack, ft_lstnew(nbr));
+	}
+	return (stack);
+}
+
+int	check_limits(char **nums)
+{
+	int	i;
+
+	i = 0;
+	while (nums[i])
+	{
+		if (nums[i][0] == '-' && ft_strlen(nums[i]) > 10 && ft_strncmp("-2147483648", nums[i], 12) < 0)
+			return (0);
+		else if ((ft_strlen(nums[i]) > 9 && ft_strncmp("2147483647", nums[i], 11) < 0) || (nums[i][0] != '-' && ft_strlen(nums[i]) > 10))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	validator(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	if (argc < 2 || !check_limits(&argv[1]))
+		return (0);
+	while (i < argc)
+	{
+		if (!ft_isnum(argv[i]))
+			return (0);
+		j = i + 1;
+		while (j < argc)
+		{
+			if (!ft_strncmp(argv[i], argv[j], 12))
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
 
 int	main(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
-	ft_printf("Hello World!");
+	t_list *a;
+	
+	if (!validator(argc, argv))
+		return (write(2, "Error1\n", 7));
+	a = converter(argc, argv);
+	if (!a)
+		return (write(2, "Error2\n", 7));
+	while (a)
+	{
+		ft_printf("->%d \n", *(int *)a->content);
+		a = a->next;
+	}
 	return (1);
 }
