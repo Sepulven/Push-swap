9861164cb0bea716197bb86530c988590a572354
Arthur Aguiar <sepulven40028922@gmail.com>
2023-05-03 10:29:31 +0100

    Error from valgrind solved.

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: asepulve <asepulve@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 14:29:23 by asepulve          #+#    #+#             */
-/*   Updated: 2023/05/02 02:23:01 by asepulve         ###   ########.fr       */
+/*   Updated: 2023/05/03 10:25:54 by asepulve         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,42 +29,55 @@ void	sort_sub_stack(t_list **a, t_list **b, t_sort_n stat)
 	while (r_a_tip-- > 0)
 		rra(a);
 }
-
-static void	init_stacks_size(int *a, int*b, int size)
+// ! Falta me verificar se o lado da esquerda calhou um número ímpar
+// * Acho que isto ja esta feito dado que 
+void	init_stacks_size(int *range, int size)
 {
-	a[0] = size / 2;
-	b[0] = size / 2 + (size % 2 != 0);
-	a[1] = a[0] / 2;
-	b[1] = b[0] / 2;
-	a[2] = a[1] / 2;
-	b[2] = b[1] / 2;
+	// * Supostamente devo colocar na ordem em que vamos utilizando.
+	// * Para duas stack devemos ter aproxidamente 2 + 4 + 8 ranges.
+	// * Dois primeiros stacks
+	range[0] = size / 2; // * Numeros pares terão parte menor
+	range[1] = size / 2 + (size % 2 != 0);
+	// * Os quatros do meio
+	range[2] = range[0] / 2;
+	range[3] = range[0] / 2 + (range[0] % 2 != 0);
+	range[4] = range[1] / 2;
+	range[5] = range[1] / 2 + (range[1] % 2 != 0);
+	// * Os oito ultimos
+	range[6] = range[2] / 2;
+	range[7] = range[2] / 2 + (range[2] % 2 != 0);
+	range[8] = range[3] / 2;
+	range[9] = range[3] / 2 + (range[3] % 2 != 0);
+	range[10] = range[4] / 2;
+	range[11] = range[4] / 2 + (range[4] % 2 != 0);
+	range[12] = range[5] / 2;
+	range[13] = range[5] / 2 + (range[5] % 2 != 0);
 }
 
 void	push_swap(t_list **a, t_list **b, int size)
 {
 	int	i;
-	int	stack_a[3];
-	int	stack_b[3];
+	int	range[14];
 
-	init_stacks_size(stack_a, stack_b, size);
+	(void)i;
+	(void)range;
+	init_stacks_size(range, size);
 	split_stack_diviser(a, b, (t_stat){'a', 'c', size});
-	diviser_tips(a, b, (t_stat){'a', 'c', stack_b[1]});
-	diviser_tips(a, b, (t_stat){'a', 'c', stack_b[1] + (stack_b[0] % 2 != 0)});
-	diviser_tips(a, b, (t_stat){'b', 'd', stack_b[2]});
+	diviser_tips(a, b, (t_stat){'a', 'c', range[4]});
+	diviser_tips(a, b, (t_stat){'a', 'c', range[5]});
+	diviser_tips(a, b, (t_stat){'b', 'd', range[10]});
 	i = 0;
-	while (i++ < stack_b[2] + (stack_b[1] % 2 != 0))
+	while (i++ < range[11])
 		rra(a);
-	diviser_tips(a, b, (t_stat){'b', 'd', stack_b[2] + (stack_b[1] % 2 != 0)});
-	diviser_tips(a, b, (t_stat){'b', 'd', stack_b[2]});
+	diviser_tips(a, b, (t_stat){'b', 'd', range[11]});
+	diviser_tips(a, b, (t_stat){'b', 'd', range[12]});
 	i = 0;
-	while (i++ < stack_b[2] + (stack_b[1] % 2 != 0))
+	while (i++ < range[13])
 		rra(a);
-	diviser_tips(a, b, (t_stat){'b', 'd', stack_b[2] + (stack_b[1] % 2 != 0)});
-	split_stack_diviser(a, b, (t_stat){'a', 'c', stack_a[0] + (size % 2 != 0)});
-	diviser_tips_sorted(a, b, (t_stat){'b', 'c', \
-	stack_a[1] + (stack_a[0] % 2 != 0)});
-	sort_sub_stack(a, b, (t_sort_n){size, 0, \
-	'b', 'g', ft_lstsize(*b)});
+	diviser_tips(a, b, (t_stat){'b', 'd', range[13]});
+	split_stack_diviser(a, b, (t_stat){'a', 'c', range[0]});
+	diviser_tips_sorted(a, b, (t_stat){'b', 'c', range[2]});
+	sort_sub_stack(a, b, (t_sort_n){size, 0, 'b', 'g', ft_lstsize(*b)});
 }
 
 int	main(int argc, char *argv[])
